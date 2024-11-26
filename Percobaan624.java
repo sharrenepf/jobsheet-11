75c806e260ec8d0f7648abadd142a30a27400b64
Sharren <sharrenepf@gmail.com>
2024-11-26 22:50:33 +0700

    percobaan 5

---
+++ Percobaan624.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Percobaan624 {
+
+    static int hitungLuas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int p, l, t, L, vol;
+
+        System.out.print("Masukkan panjang : ");
+        p = input.nextInt();
+        System.out.print("Masukkan lebar : ");
+        l = input.nextInt();
+        System.out.print("Masukkan tinggi : ");
+        t = input.nextInt();
+
+        L = hitungLuas(p, l);
+        System.out.println("Luas Persegi Panjang adalah " + L);
+
+        vol = hitungVolume(t, p, L);
+        System.out.println("Volume balok adalah " + vol);
+    }
+}
